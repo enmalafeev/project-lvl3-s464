435555c16f162224c90a6b13ba5845853ca700d3
enmalafeev <malafeev.en@yandex.ru>
2019-09-25 18:01:56 +0300

    add validation search input

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@babel/polyfill';
+import validator from 'validator';
+import WatchJS from 'melanke-watchjs';
+
+const input = document.querySelector('.form-control');
+const { watch } = WatchJS;
+
+const state = {
+  input: {
+    url: '',
+    isValid: false,
+  },
+};
+
+watch(state.input, 'url', () => {
+  if (state.input.isValid) {
+    input.classList.remove('border-danger');
+  } else {
+    input.classList.add('border-danger');
+  }
+});
+
+input.addEventListener('input', (e) => {
+  state.input.url = e.target.value;
+  if (state.input.url === '') {
+    state.input.isValid = true;
+  } else if (validator.isURL(state.input.url)) {
+    state.input.isValid = true;
+  } else {
+    state.input.isValid = false;
+  }
+});
