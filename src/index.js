3f6a164ec3ebd47fafc0ae0df12cf75c07ce5335
enmalafeev <malafeev.en@yandex.ru>
2019-10-10 22:28:07 +0300

    move parser into seperate file

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 import WatchJS from 'melanke-watchjs';
 import $ from 'jquery';
 import _ from 'lodash';
+import parseFeed from './parser';
 
 const app = () => {
   const input = document.querySelector('.form-control');
@@ -29,25 +30,6 @@ const app = () => {
       feedLinks: [],
       subscribedFeeds: [],
     },
-  };
-
-  const parseFeed = (xml) => {
-    const domParser = new DOMParser();
-    const doc = domParser.parseFromString(`${xml.data}`, 'application/xml');
-    const channel = doc.querySelector('channel');
-    const title = channel.querySelector('title').textContent;
-    const description = channel.querySelector('description').textContent;
-    const items = channel.querySelectorAll('item');
-    const itemsList = [...items].map((item) => {
-      const pubDate = new Date(item.querySelector('pubDate').textContent);
-      const itemTitle = item.querySelector('title').textContent;
-      const itemDescription = item.querySelector('description').textContent;
-      const itemLink = item.querySelector('link').textContent;
-      return {
-        pubDate, itemTitle, itemDescription, itemLink,
-      };
-    });
-    return { title, description, itemsList };
   };
 
   const validateDublicates = url => state.feed.subscribedFeeds.some(el => el === url);
