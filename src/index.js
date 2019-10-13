9a927e60b1a143c3cb76ec346466ebefe5f632ca
enmalafeev <malafeev.en@yandex.ru>
2019-10-13 23:17:21 +0300

    add error processing

--- src/index.js
+++ src/index.js
@@ -16,6 +16,26 @@ const app = () => {
   const feedsNode = document.querySelector('.feeds');
   const { watch } = WatchJS;
   const corsOrigin = 'http://cors-anywhere.herokuapp.com/';
+
+  const errors = {
+    empty: 'Input field should not to be empty',
+    dublicate: 'This feed is already exist',
+    invalid: 'Please enter valid RSS-feed',
+    network: 'Network error, please try again',
+  };
+
+  const showError = (state) => {
+    const errorNode = document.getElementById('showErr');
+    const errorText = errors[state.error];
+    if (errorText) {
+      errorNode.textContent = errorText;
+      errorNode.classList.toggle('d-none');
+    } else {
+      errorNode.textContent = state.error;
+      errorNode.classList.toggle('d-none');
+    }
+  };
+
   const state = {
     input: {
       url: '',
@@ -27,6 +47,7 @@ const app = () => {
       feedLinks: [],
       subscribedFeeds: [],
     },
+    error: null,
   };
 
   const validateDublicates = url => state.feed.subscribedFeeds.some(el => el === url);
@@ -65,6 +86,8 @@ const app = () => {
     linksNode.innerHTML = linksArr;
   });
 
+  watch(state, 'error', () => showError(state));
+
   $('#showDescription').on('show.bs.modal', (event) => {
     const button = $(event.relatedTarget);
     const recipient = button.data('description');
@@ -76,12 +99,16 @@ const app = () => {
     state.input.url = e.target.value;
     if (state.input.url === '') {
       state.formState = 'empty';
+      state.error = 'empty';
     } else if (validateDublicates(state.input.url)) {
       state.formState = 'invalid';
+      state.error = 'dublicate';
     } else if (!validator.isURL(state.input.url)) {
       state.formState = 'invalid';
+      state.error = 'invalid';
     } else {
       state.formState = 'valid';
+      state.error = null;
     }
   });
 
@@ -100,7 +127,7 @@ const app = () => {
         const maxPubDate = _.max(dataFeed.itemsList.map(({ pubDate }) => pubDate));
         setTimeout(() => updatePosts(link, maxPubDate), 5000);
       })
-      .catch(err => console.log(err));
+      .catch(() => { state.error = 'network'; });
   });
 };
 
